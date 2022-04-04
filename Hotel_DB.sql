37b3c918d4b76dba6f4a7478d557a221c561d7d7
joakim-abra <89795849+joakim-abra@users.noreply.github.com>
2022-04-04 10:33:19 +0200

    1st table

--- Hotel_DB.sql
+++ Hotel_DB.sql
@@ -3,3 +3,17 @@ GO
 
 USE Hotel
 GO
+
+CREATE TABLE Customer(
+
+    ID INT IDENTITY PRIMARY KEY,
+
+    first_name NVARCHAR(20),
+
+    last_name NVARCHAR(50),
+
+    email NVARCHAR(50),
+
+    phone_number INT FOREIGN KEY
+
+)
