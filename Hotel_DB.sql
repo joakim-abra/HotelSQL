70e1474c8ab9f3a59da1dc2450332a1bb9509806
joakim-abra <89795849+joakim-abra@users.noreply.github.com>
2022-04-05 12:12:32 +0200

    vette

--- Hotel_DB.sql
+++ Hotel_DB.sql
@@ -4,13 +4,6 @@ GO
 USE Hotel
 GO
 
-
---USE Population
---GO
-
---DROP DATABASE Hotel;
---GO
-=======
 -- USE Population
 -- GO
 
@@ -18,7 +11,6 @@ GO
 -- GO
 
 
-<<<<<<< HEAD
 --VIEW ALLA RUM OCH TYP
 
 -- SELECT r.room_NR, rt.name, rt.nr_of_beds, rt.balcony, r.extra_bed FROM Room r
@@ -29,40 +21,46 @@ GO
 
 
 
-=======
->>>>>>> 44e88601db739ee6fc47d73aa7436e92828b0e5b
 CREATE TABLE Employees(
     employee_ID INT IDENTITY PRIMARY KEY,
     first_name NVARCHAR(20),
     last_name NVARCHAR(50),
     position NVARCHAR(20)
-);
+)
 GO
 
 
 CREATE TABLE Customer(
+
     ID INT IDENTITY PRIMARY KEY,
+
     first_name NVARCHAR(20),
+
     last_name NVARCHAR(50),
+
     email NVARCHAR(50),
+
     phone_number NVARCHAR(30),
+
     street_address NVARCHAR(50),
+
     city NVARCHAR(50),
-<<<<<<< HEAD
 
     postal_code NVARCHAR(20),
 
-=======
-    postal_code INT,
->>>>>>> 44e88601db739ee6fc47d73aa7436e92828b0e5b
     country NVARCHAR(50),
+
     is_contact BIT NOT NULL DEFAULT 0
+
 );
 GO
 
 CREATE TABLE creditcard(
-    card_type_ID INT IDENTITY PRIMARY KEY,
+
+    card_type_ID INT PRIMARY KEY,
+
     card_type NVARCHAR(50),
+
 );
 GO
 
@@ -79,17 +77,14 @@ CREATE TABLE Room(
     room_NR INT IDENTITY PRIMARY KEY,
     room_type_id INT FOREIGN KEY REFERENCES Room_type(room_type_id),
     extra_bed INT NOT NULL DEFAULT 0
+
 );
 GO
 
 CREATE TABLE Guest_booking(
     id INT IDENTITY PRIMARY KEY,
     customer_id INT FOREIGN KEY REFERENCES Customer (ID)
-
-);
-=======
 )
-
 GO
 
 -- CREATE TABLE Rooms_booked(
@@ -105,6 +100,7 @@ CREATE TABLE Booking(
     room_id INT FOREIGN KEY REFERENCES Room(room_NR),
     guest_booking_id INT FOREIGN KEY REFERENCES Guest_booking(id),
 --- REFERENS TILL TABELL MED BOKANDE GÄSTER
+
     num_of_night INT,
     check_in_date DATETIME,
     check_out_date DATETIME,
@@ -116,18 +112,20 @@ CREATE TABLE Booking(
 );
 GO
 
+
 CREATE TABLE room_bill(
     amount DECIMAL,
     bill_id INT IDENTITY PRIMARY KEY,
     --total_discount NVARCHAR(10),
+
     payment_booking_id INT FOREIGN KEY REFERENCES Booking (booking_id)
 );
 GO
 
-CREATE TABLE Rebate(
-    rebate_id INT IDENTITY PRIMARY KEY,
-    rebate_code NVARCHAR(30),
-    rebate_amount INT
+CREATE TABLE discount(
+    discount_id INT IDENTITY PRIMARY KEY,
+    discount_code NVARCHAR(30),
+    discount_amount INT
 );
 GO
 
@@ -142,12 +140,14 @@ CREATE TABLE total_booking_bill
 );
 GO
 
+
 CREATE TABLE Messages(
     message_id INT IDENTITY PRIMARY KEY,
     customer_id INT FOREIGN KEY REFERENCES Customer(ID),
     comment NVARCHAR(500),
     employee_ref INT FOREIGN KEY REFERENCES Employees(employee_ID)
-);
+
+)
 GO
 
 
@@ -156,6 +156,7 @@ CREATE TABLE Feedback(
     comment NVARCHAR(500),
     score INT,
     booking INT FOREIGN KEY REFERENCES Booking(booking_id)
+
 );
 GO
 
@@ -164,6 +165,5 @@ log_id INT IDENTITY PRIMARY KEY,
 booking_id INT FOREIGN KEY REFERENCES booking(booking_id),
 log_check_in DATETIME,
 log_check_out DATETIME
-);
+)
 GO
-
